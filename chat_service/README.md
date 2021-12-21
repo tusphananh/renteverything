a7fb970e197de5d88680fd56e9e5bb9fc3df36c1
tusphananh <tusphananh@gmail.com>
2021-12-22 00:36:15 +0700

    commit

---
+++ chat_service/README.md
@@ -0,0 +1,34 @@
+# **README**
+
+## Introduction
+
+#
+
+This service is used to send and receive messages between users. Connecting with database and save messages.
+
+## Installation
+
+#
+
+> Before you can use this service, you need to install api_service and run docker-compose to deploy the database.
+
+1. Install packages:
+
+```bash
+npm install
+```
+
+2. Run production:
+
+```bash
+npm run production
+```
+
+## Libraries
+
+#
+
+> This service uses the following libraries:
+
+- [Socket.io](https://socket.io/)
+- [Express](https://expressjs.com/)
--- chat_service/src/index.ts
+++ chat_service/src/index.ts
@@ -1,8 +1,50 @@
-import express from 'express';
-// rest of the code remains same
-const app = express();
-const PORT = 8000;
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+require('dotenv').config();
 
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+const PORT = process.env.PORT;
+const httpServer = createServer();
+
+interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+    hello: () => void;
+}
+
+interface InterServerEvents {
+    ping: () => void;
+}
+
+interface SocketData {
+    name: string;
+    age: number;
+}
+
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
+    cors: {
+        origin: `${process.env.CORS_ORIGIN}`,
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
+
+io.on("connection", (socket: Socket) => {
+    console.log("New client connected with id: ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected with id: ", socket.id);
+    });
+});
+
+
+
+
+
+httpServer.listen(PORT, () => {
+    console.log(`Search service listening on port ${PORT}`);
+    console.log(`CORS origin: ${process.env.CORS_ORIGIN}`);
 });
