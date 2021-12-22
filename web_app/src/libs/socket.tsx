05e4134083f3366d2fc5bb30ddf3d657e32a2a0a
tusphananh <tusphananh@gmail.com>
2021-12-22 10:13:51 +0700

    commit

--- web_app/src/libs/socket.tsx
+++ web_app/src/libs/socket.tsx
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 import {
   ClientToServerEvents,
   ServerToClientEvents,
-} from "../constants/SocketInterface";
+} from "../constants/SocketConstants";
 
 // please note that the types are reversed
 // because the server is the client and the client is the server
